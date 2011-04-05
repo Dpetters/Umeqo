6412d4c40cc012ba7a60e00a5a7bb9e9b8437bb0
Dmitrij Petters <dpetters@mit.edu>
2011-04-05 10:44:28 -0400

    Upgraded to Django 1.3, tiny change to setting.py, added django.db.backends. to engine

--- settings.py
+++ settings.py
@@ -146,7 +146,7 @@ except ImportError:
     
     DATABASES = {
         'default': {
-            'ENGINE': '', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': '',         # Or path to database file if using sqlite3.
             'USER': '',         # Not used with sqlite3.
             'PASSWORD': '',          # Not used with sqlite3.
