b7d21cc0f38e214c4035899e820cdc9fb90aa11e
Joshua Ma <me@joshma.com>
2011-04-22 18:08:50 -0400

    oops forgot to add new files

---
+++ settings_prod.py
@@ -0,0 +1,22 @@
+CACHE_BACKEND = 'memcached://127.0.0.01:11211/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',    # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'umeqo_main',                         # Or path to database file if using sqlite3.
+        'USER': 'root',                         # Not used with sqlite3.
+        'PASSWORD': 'Jamb4Juic3',                     # Not used with sqlite3.
+        'HOST': '',                         # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                         # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
