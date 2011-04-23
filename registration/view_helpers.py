d3fa9cefe2c1c6e69ea58761b4822a85739636fb
Dmitrij Petters <dpetters@mit.edu>
2011-04-22 22:19:03 -0400

    .

--- registration/view_helpers.py
+++ registration/view_helpers.py
@@ -7,8 +7,9 @@ import re
 
 def modify_redirect(request, redirect_to=None):
     if not redirect_to or ' ' in redirect_to or '//' in redirect_to and re.match(r'[^\?]*//', redirect_to):
-        if hasattr(request, "employer"):
-            redirect_to = "/employer/%s/" % request.user
-        elif hasattr(request, "student"):
-            redirect_to = "/student/%s/" % request.user
+        if hasattr(request.user, 'employer'):
+            redirect_to = "/employer/"
+        elif hasattr(request.user, "student"):
+            redirect_to = "/student/"
+    print redirect_to
     return redirect_to
