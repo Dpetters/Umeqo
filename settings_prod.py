329aa40626d23b9dab7aad78e0036e2a8802c493
Joshua Ma <me@joshma.com>
2011-04-23 16:30:47 -0400

    fixed typo in setup_local_database

--- settings_prod.py
+++ settings_prod.py
@@ -1,5 +1,8 @@
 CACHE_BACKEND = 'memcached://127.0.0.01:11211/'
 
+DEBUG = True 
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',    # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
