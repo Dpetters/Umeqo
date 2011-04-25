547d3425d39fa0ba4f13c1d466944bf33e14912a
Dmitrij Petters <dpetters@mit.edu>
2011-04-25 02:03:00 -0400

    Reorganized employer_filtering.js

--- static/javascript/employer/employer_filtering.js
+++ static/javascript/employer/employer_filtering.js
@@ -24,32 +24,137 @@ $(document).ready( function() {
 	var campus_organizations = new Array()
 	var must_be_older_than_18 = false;
 
-	// Listen for a campus organization info
-	$(".campus_org_link").live('click', campus_org_link_click);
 
-	var initiate_search = function() {
+	function handle_multiselect_open_in_accordion(event, ui) {
+			var parent = $('#' + $(event.target).attr('id')).parents(".ui-accordion-content");
+			var multiselect = $('#' + $(event.target).attr('id')).nextAll(".ui-multiselect-menu");
+			var new_height = parseInt($(multiselect).css('height'), 10) + parseInt($(multiselect).css('top'), 10);		
+			$(parent).css('height', new_height);
+	};
+	
+	function handle_multiselect_close_in_accordion(event, ui) {
+			$('#' + $(event.target).attr('id')).parents(".ui-accordion-content").css("height", "");
+	};
+
+	function campus_org_link_click() {
+		alert("Campus org link click func not implemented!")
+	};
+	
+	function initiate_search() {
 		query = $("#query_field").val().replace(/[^a-z\d ]+/ig,'');
 		initiate_ajax_call();
 	};
+
+	function initiate_ajax_call() {
+		$("#results_section").css('opacity', 0.5);
+		$("#results_block_loader_section").css('display', 'block');
+
+		$.ajax({
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'))
+			},
+			type: 'POST',
+			url: '/employer/student-filtering/',
+			dataType: "html",
+			data: {
+				'results_per_page':20,
+				'query': query,
+				'gpa' : min_gpa,
+				'act': min_act,
+				'sat' : min_sat,
+				'page': page,
+				'ordering': ordering,
+				'results_per_page': results_per_page,
+				'courses' : courses
+			},
+			success: function (data) {
+				$('#results_section').html(data);
+
+				// Hide all extra details except for the first
+				$(".student_detailed_info").hide();
+				$("#student_detailed_info_0").show();
+				$(".view_student_detailed_info_link_cell #0").text("Hide Details");
+
+				$(".student_drag_anchor").draggable({
+					'revert':true,
+					helper: function(event) {
+						return $("<div class='student_drag_div'>" + $(this).parent(".student_checkbox_cell").next().find(".student_name").text() + "</div>");
+					},
+					'revertDuration': 100,
+					'drag': function(event, ui) {
+						$("#resume_book_block .side_block_content").html("<p>Drop student's name here!</p>");
+					}
+				});
+
+				// Bring the opacity back to normal and hide the ajax loader
+				$("#results_section").css('opacity', 1);
+				$("#results_block_loader_section").css('display', 'none');
+			}
+		});
+	};
+	
+	
+	// Listen for a campus organization info link click
+	$(".campus_org_link").live('click', campus_org_link_click);
 	
 	// Listen for a new search
-	$("#search_form_submit_button").live('click', initiate_search);
+	$("#search_form_submit_button").click(initiate_search);
 
-	// Listen for Results per page sections
+	// Listen for results per page change
 	$("#id_results_per_page").change( function() {
 		results_per_page = $("#id_results_per_page option:selected").val();
 		initiate_ajax_call();
 	});
-	// Listen for Ordering Changes
+	
+	// Listen for result ordering change
 	$("#id_ordering").change( function() {
 		ordering = $("#id_ordering option:selected").val();
 		initiate_ajax_call();
 	});
-	// Listen for page changes
+	
+	// Listen for result page change
 	$(".page_link").live('click', function() {
 		page = $(this).attr("id").substring(5);
 		initiate_ajax_call();
 	});
+
+	// Listen for result detailed info toggle
+	$(".view_student_detailed_info_link").live('click', function() {
+		var selector = ".view_student_detailed_info_link_cell #" + this.id;
+
+		if ($(selector).text() === "Hide Details")
+			$(selector).text("View Details");
+		else
+			$(selector).text("Hide Details");
+		$('#student_detailed_info_'+ this.id).slideToggle('slow');
+	});
+
+	// Listen for mater checkbox click
+	$("#results_menu_checkbox").live('click', function() {
+		if($("#results_menu_checkbox").attr('checked')==false) {
+			$(".student_checkbox:checked").each( function() {
+				$(this).attr('checked', false);
+			});
+		} else {
+			$(".student_checkbox").not(':checked').each( function() {
+				$(this).attr('checked', true);
+			});
+		}
+	});
+	// Listen for hide/view all details click
+	$("#results_menu_toggle_details").live('click', function() {
+		if( $("#results_menu_toggle_details span").text()=="View All Details") {
+			$(".view_student_detailed_info_link").text("Show Details");
+			$(".student_detailed_info").show('slow');
+			$("#results_menu_toggle_details span").text("Hide All Details");
+		} else {
+			$(".view_student_detailed_info_link").text("Hide Details");
+			$(".student_detailed_info").hide('slow');
+			$("#results_menu_toggle_details span").text("View All Details");
+		}
+	});
+	
+	
 	$("#id_student_list").multiselect({
 		height:138,
 		header:false,
@@ -57,17 +162,6 @@ $(document).ready( function() {
 		selectedList: 1,
 		multiple: false
 	});
-
-	var handle_multiselect_open_in_accordion = function(event, ui) {
-			var parent = $('#' + $(event.target).attr('id')).parents(".ui-accordion-content");
-			var multiselect = $('#' + $(event.target).attr('id')).nextAll(".ui-multiselect-menu");
-			var new_height = parseInt($(multiselect).css('height'), 10) + parseInt($(multiselect).css('top'), 10);		
-			$(parent).css('height', new_height);
-	};
-	
-	var handle_multiselect_close_in_accordion = function(event) {
-			$('#' + $(event.target).attr('id')).parents(".ui-accordion-content").css("height", "");
-	};
 	
 	$("#id_majors").multiselect({
 		noneSelectedText: 'Filter By Major',
@@ -178,7 +272,6 @@ $(document).ready( function() {
 		close: handle_multiselect_close_in_accordion
 	});
 	
-	
 	$("#id_ordering").multiselect({
 		height:47,
 		header:false,
@@ -199,16 +292,6 @@ $(document).ready( function() {
 		close: handle_multiselect_close_in_accordion
 	});
 
-	// Result Collapse
-	$(".view_student_detailed_info_link").live('click', function() {
-		var selector = ".view_student_detailed_info_link_cell #" + this.id;
-
-		if ($(selector).text() === "Hide Details")
-			$(selector).text("View Details");
-		else
-			$(selector).text("Hide Details");
-		$('#student_detailed_info_'+ this.id).slideToggle('slow');
-	});
 	// GPA Slider
 	$("#gpa_filter_section div").slider({
 		min: 0,
@@ -267,6 +350,8 @@ $(document).ready( function() {
 	$("#id_act").val($("#act_filter_section div").slider("value"));
 	$("#id_sat").val($("#sat_filter_section div").slider("value"));
 
+	
+	// Make the resume block into a droppable area
 	$("#resume_book_block .side_block_content").droppable({
 		activeClass: "add_to_resume_book_area_active",
 		hoverClass: "add_to_resume_book_area_hover",
@@ -279,83 +364,7 @@ $(document).ready( function() {
 		}
 	});
 		
-	function campus_org_link_click() {
-
-	};
-
-
-	var initiate_ajax_call = function() {
-		$("#results_section").css('opacity', 0.5);
-		$("#results_block_loader_section").css('display', 'block');
-
-		$.ajax({
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'))
-			},
-			type: 'POST',
-			url: '/employer/student-filtering/',
-			dataType: "html",
-			data: {
-				'results_per_page':20,
-				'query': query,
-				'gpa' : min_gpa,
-				'act': min_act,
-				'sat' : min_sat,
-				'page': page,
-				'ordering': ordering,
-				'results_per_page': results_per_page,
-				'courses' : courses
-			},
-			success: function (data) {
-				$('#results_section').html(data);
-
-				// Hide all extra details except for the first
-				$(".student_detailed_info").hide();
-				$("#student_detailed_info_0").show();
-				$(".view_student_detailed_info_link_cell #0").text("Hide Details");
-
-				$(".student_drag_anchor").draggable({
-					'revert':true,
-					helper: function(event) {
-						return $("<div class='student_drag_div'>" + $(this).parent(".student_checkbox_cell").next().find(".student_name").text() + "</div>");
-					},
-					'revertDuration': 100,
-					'drag': function(event, ui) {
-						$("#resume_book_block .side_block_content").html("<p>Drop student's name here!</p>");
-					}
-				});
-
-				// Bring the opacity back to normal and hide the ajax loader
-				$("#results_section").css('opacity', 1);
-				$("#results_block_loader_section").css('display', 'none');
-			}
-		});
-	};
-	// Handle the selection of all checkboxes
-	$("#results_menu_checkbox").live('click', function() {
-		if($("#results_menu_checkbox").attr('checked')==false) {
-			$(".student_checkbox:checked").each( function() {
-				$(this).attr('checked', false);
-			});
-		} else {
-			$(".student_checkbox").not(':checked').each( function() {
-				$(this).attr('checked', true);
-			});
-		}
-	});
-	// Hide/Show All Details Menu Button
-	$("#results_menu_toggle_details").live('click', function() {
-		if( $("#results_menu_toggle_details span").text()=="View All Details") {
-			$(".view_student_detailed_info_link").text("Show Details");
-			$(".student_detailed_info").show('slow');
-			$("#results_menu_toggle_details span").text("Hide All Details");
-		} else {
-			$(".view_student_detailed_info_link").text("Hide Details");
-			$(".student_detailed_info").hide('slow');
-			$("#results_menu_toggle_details span").text("View All Details");
-		}
-	});
-	// Have side block area follow scrolling
+	// Set up side block area scrolling
 	var el = $('#side_block_area');
 	var elpos_original = el.offset().top;
 	$(window).scroll( function() {
@@ -373,12 +382,14 @@ $(document).ready( function() {
 			}, 600, 'easeInOutExpo');
 		}
 	});
-	// Filtering block is an accordion
+	
+	// Make the filtering block an accordion
 	a = $("#filtering_accordion").accordion({
 		autoHeight: false,
 		clearStyle: true,
 		collapsible: true
 	});
+
 
 	// Make the first ajax call for results automatically
 	initiate_ajax_call();
