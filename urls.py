2d17760bd71b6603aaa078a4f4cea05eeaa9fd74
Joshua Ma <me@joshma.com>
2011-04-25 00:21:51 -0400

    added email at bottom

--- urls.py
+++ urls.py
@@ -17,16 +17,15 @@ import haystack
 haystack.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATICFILES_DIRS[0] }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATICFILES_DIRS[0] }),
     )
 
 if not settings.LAUNCHED and not settings.DEBUG:
     urlpatterns += patterns('',
-    (r'^', 'core.views.landing_page'),
+        (r'^', 'core.views.landing_page'),
     )
 
 urlpatterns += patterns('',
-    (r'^', 'core.views.landing_page'),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     (r'^admin/', include(admin.site.urls)),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
