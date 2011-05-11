eac9f424a587b549db5ff10933e6061d4b2ff86a
Joshua Ma <me@joshma.com>
2011-05-10 20:56:24 -0400

    changed typos in settings

--- settings_local_template.py
+++ settings_local_template.py
@@ -12,11 +12,11 @@ ROOT = os.path.dirname(os.path.realpath("__file__"))
 
 CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
 
-LOCAL_SETTINGS_APPS = ('employer,'
+LOCAL_SETTINGS_APPS = ('employer',
                        'student',
                        'events',
                        'registration',
-                       'auth.user',
+                       'user',
                        )
 
 DATABASES = {
