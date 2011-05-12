bbdb61786c0d5bc20177c444264a146f815b56fe
Dmitrij Petters <dpetters@mit.edu>
2011-05-11 18:56:52 -0700

    fixed some commit_local_data bugs. Testing it was worth it after all

--- commit_local_data.py
+++ commit_local_data.py
@@ -24,26 +24,28 @@ def delete_contents(directory):
             os.rmdir(os.path.join(root, name))
 
 # Delete old search index files
-if os.path.exists("./local_data/xapian_index/"):
-    delete_contents("./local_data/xapian_index/")
-    os.rmdir("./local_data/xapian_index/")
+if os.path.exists("./local_data/local_xapian_index/"):
+    delete_contents("./local_data/local_xapian_index/")
+    os.rmdir("./local_data/local_xapian_index/")
 
 # Delete the old submitted resumes
-if os.path.exists("./local_data/media/submitted_resumes/"):
-    delete_contents("./local_data/media/submitted_resumes/")
-    os.rmdir("./local_data/media/submitted_resumes/")
+if os.path.exists("./local_data/media/local_submitted_resumes/"):
+    delete_contents("./local_data/media/local_submitted_resumes/")
+    os.rmdir("./local_data/media/local_submitted_resumes/")
 
 # Delete the old submitted user images
-if os.path.exists("./local_data/media/submitted_user_images/"):
-    delete_contents("./local_data/media/submitted_user_images/")
-    os.rmdir("./local_data/media/submitted_user_images/")
+if os.path.exists("./local_data/media/local_submitted_user_images/"):
+    delete_contents("./local_data/media/local_submitted_user_images/")
+    os.rmdir("./local_data/media/local_submitted_user_images/")
 
-shutil.copytree("./xapian_index/", "./local_data/xapian_index/")
-shutil.copytree("./media/submitted_resumes/", "./local_data/media/submitted_resumes/")
-shutil.copytree("./media/submitted_user_images/", "./local_data/media/submitted_user_images/")
+shutil.copytree("./xapian_index/", "./local_data/local_xapian_index/")
+shutil.copytree("./media/submitted_resumes/", "./local_data/media/local_submitted_resumes/")
+shutil.copytree("./media/submitted_user_images/", "./local_data/media/local_submitted_user_images/")
 
 for app in settings.LOCAL_SETTINGS_APPS:
+    # For some reason just putting user down on loaddata works but for dumpdata you need auth.user
     if app == "user":
-        app = "auth.user"
-    p = subprocess.Popen("python manage.py dumpdata " + app + " --indent=1 > ./local_data/fixtures/local_" + app + "_data.json", shell=True)
+        p = subprocess.Popen("python manage.py dumpdata auth.user --indent=1 > ./local_data/fixtures/local_user_data.json", shell=True)
+    else:
+        p = subprocess.Popen("python manage.py dumpdata " + app + " --indent=1 > ./local_data/fixtures/local_" + app + "_data.json", shell=True)
     p.wait()
