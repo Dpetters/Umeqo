7fa96aa045bbcdf11d8e4cb27913bc30dd22b281
Dmitrij Petters <dpetters@mit.edu>
2011-05-12 14:35:49 -0400

    Changed the search field from keywords to text. Also got rid of the first name and last name fields and instead put them in the template where they belong

--- student/search_indexes.py
+++ student/search_indexes.py
@@ -8,8 +8,6 @@ from haystack import indexes, site
 from student.models import Student
 
 class StudentIndex(indexes.RealTimeSearchIndex):
-    keywords = indexes.CharField(use_template=True, document=True)
-    first_name = indexes.CharField(model_attr="first_name", null=True)
-    last_name = indexes.CharField(model_attr="last_name", null=True)
+    text = indexes.CharField(use_template=True, document=True)
 
 site.register(Student, StudentIndex)
