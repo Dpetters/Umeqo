8635fe8598280aa328537b9d647a2db83986fade
Joshua Ma <me@joshma.com>
2011-05-14 01:14:29 -0400

    updated indexes, will change more probs

--- events/search_indexes.py
+++ events/search_indexes.py
@@ -8,7 +8,7 @@ from haystack import indexes, site
 from events.models import Event
 
 class EventIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(use_template=True, document=True)
+    text = indexes.EdgeNgramField(use_template=True, document=True)
     start_datetime = indexes.DateTimeField(model_attr='start_datetime',null=True)
     end_datetime = indexes.DateTimeField(model_attr='end_datetime')
     type = indexes.CharField(model_attr='type')
