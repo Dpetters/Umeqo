73463cbc705ac070bcf4f64a412d2066df7725c7
Dmitrij Petters <dpetters@mit.edu>
2011-05-14 16:44:26 -0400

    Updated create_local_data.py and commit_local_data.py

--- commit_local_data.py
+++ commit_local_data.py
@@ -8,6 +8,12 @@
 This script is meant to dump newly-added fake non-fixturable data to what we already have.
 """
 
+c  = raw_input('Make sure that you ran "create_local_data.py" then created the new local data, and only then are running this script in that order.\n\
+Otherwise you risk overwriting existing local data. Press lowercase "y" to continue.\n\'')
+
+if not c=='y':
+    exit()
+    
 import os, subprocess, shutil
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
@@ -23,27 +29,43 @@ def delete_contents(directory):
         for name in dirs:
             os.rmdir(os.path.join(root, name))
 
-# Delete old search index files
+# Delete old local_data search index files. Also delete the directory if it exists because
+# copytree below will throw a fit if it already exists
 if os.path.exists("./local_data/local_xapian_index/"):
     delete_contents("./local_data/local_xapian_index/")
     os.rmdir("./local_data/local_xapian_index/")
 
-# Delete the old submitted resumes
+if not os.path.exists(settings.HAYSTACK_XAPIAN_PATH):
+    os.mkdir(settings.HAYSTACK_XAPIAN_PATH)
+
+shutil.copytree(settings.HAYSTACK_XAPIAN_PATH, "./local_data/local_xapian_index/")
+
+# Delete the old local_data submitted resumes. Also delete the directory if it exists because
+# copytree below will throw a fit if it already exists
 if os.path.exists("./local_data/media/local_submitted_resumes/"):
     delete_contents("./local_data/media/local_submitted_resumes/")
     os.rmdir("./local_data/media/local_submitted_resumes/")
 
-# Delete the old submitted user images
+submitted_resumes_path = ROOT + "/media/submitted_resumes/"
+if not os.path.exists(submitted_resumes_path):
+    os.mkdir(submitted_resumes_path)  
+
+shutil.copytree(submitted_resumes_path, "./local_data/media/local_submitted_resumes/")
+
+# Delete the old local_data submitted user images. Also delete the directory if it exists because
+# copytree below will throw a fit if it already exists
 if os.path.exists("./local_data/media/local_submitted_user_images/"):
     delete_contents("./local_data/media/local_submitted_user_images/")
     os.rmdir("./local_data/media/local_submitted_user_images/")
 
-shutil.copytree("./xapian_index/", "./local_data/local_xapian_index/")
-shutil.copytree("./media/submitted_resumes/", "./local_data/media/local_submitted_resumes/")
-shutil.copytree("./media/submitted_user_images/", "./local_data/media/local_submitted_user_images/")
+submitted_user_images_path = ROOT + "/media/submitted_user_images/"
+if not os.path.exists(submitted_user_images_path):
+    os.mkdir(submitted_user_images_path)  
+    
+shutil.copytree(submitted_user_images_path, "./local_data/media/local_submitted_user_images/")
 
 for app in settings.LOCAL_SETTINGS_APPS:
-    # For some reason just putting user down on loaddata works but for dumpdata you need auth.user
+    # For some reason just running "loaddata user" works but "dumpdata user" doesn't. You need "dumpdata auth.user"
     if app == "user":
         p = subprocess.Popen("python manage.py dumpdata auth.user --indent=1 > ./local_data/fixtures/local_user_data.json", shell=True)
     else:
--- create_local_data.py
+++ create_local_data.py
@@ -23,31 +23,41 @@ def delete_contents(directory):
         for name in dirs:
             os.rmdir(os.path.join(root, name))
 
-# Delete old search index files
+# Delete old search index files.
+# New index files get created automatically when loaddata gets run below
 if os.path.exists(settings.HAYSTACK_XAPIAN_PATH):
     delete_contents(settings.HAYSTACK_XAPIAN_PATH)
-    os.rmdir(settings.HAYSTACK_XAPIAN_PATH)
 
-# Delete the old submitted resumes
+# Delete the old submitted resumes. Also delete the directory if it exists because
+# copytree below will throw a fit if it already exists
 submitted_resumes_path = ROOT + "/media/submitted_resumes/"
 if os.path.exists(submitted_resumes_path):
     delete_contents(submitted_resumes_path)
     os.rmdir(submitted_resumes_path)
 
-# Delete the old submitted user images
+local_data_submitted_resumes_path = "./local_data/media/local_submitted_resumes/"
+if not os.path.exists(local_data_submitted_resumes_path):
+    os.mkdir(local_data_submitted_resumes_path)  
+
+shutil.copytree(local_data_submitted_resumes_path, submitted_resumes_path)
+
+# Delete the old submitted user images. Also delete the directory if it exists because
+# copytree below will throw a fit if it already exists
 submitted_user_images_path = ROOT + "/media/submitted_user_images/"
 if os.path.exists(submitted_user_images_path):
     delete_contents(submitted_user_images_path)
     os.rmdir(submitted_user_images_path)
 
-shutil.copytree("./local_data/local_xapian_index/", "./xapian_index/")
-shutil.copytree("./local_data/media/local_submitted_resumes/", "./media/submitted_resumes/")
-shutil.copytree("./local_data/media/local_submitted_user_images/", "./media/submitted_user_images/")
+local_data_submitted_user_images_path = "./local_data/media/local_submitted_resumes/"
+if not os.path.exists(local_data_submitted_user_images_path):
+    os.mkdir(local_data_submitted_user_images_path)  
+    
+shutil.copytree(local_data_submitted_user_images_path, submitted_user_images_path)
+
 
 p = subprocess.Popen("python manage.py syncdb --noinput --migrate", shell=True)
 p.wait()
 
 for app in settings.LOCAL_SETTINGS_APPS:
-    print "python manage.py loaddata ./local_data/fixtures/local_" + app + "_data.json"
     p = subprocess.Popen("python manage.py loaddata ./local_data/fixtures/local_" + app + "_data.json", shell=True)
     p.wait()
