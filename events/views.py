2d36b1f7ce5fc97ceb7c47817c7cd8b1a46a7d05
Joshua Ma <me@joshma.com>
2011-06-07 00:43:50 -0700

    moved company_logo to view

--- events/views.py
+++ events/views.py
@@ -46,7 +46,7 @@ def event_page(request, id, slug, template_name='event_page.html', extra_context
     #check slug matches event
     if event.slug!=slug:
         return HttpResponseNotFound()
-    page_url = 'http://'+settings.DOMAIN+reverse('event_page',kwargs={'id':event.id,'slug':event.slug})
+    page_url = '//'+settings.DOMAIN+reverse('event_page',kwargs={'id':event.id,'slug':event.slug})
     #google_description is the description + stuff to link back to umeqo
     google_description = event.description + '\n\nRSVP and more at %s' % page_url
     context = {
@@ -69,6 +69,9 @@ def event_page(request, id, slug, template_name='event_page.html', extra_context
         context['can_rsvp'] = True
     elif hasattr(request.user,"recruiter"):
         context['show_admin'] = True
+    
+    context['company_logo'] = "//"+settings.DOMAIN+settings.STATIC_URL+'images/company_logo_filler.png'
+    
     context.update(extra_context or {})
     return render_to_response(template_name,context,context_instance=RequestContext(request))
 
