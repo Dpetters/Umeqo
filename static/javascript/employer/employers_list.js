57055cae306b9d7c1870faafc7f17457f74dc653
Joshua Ma <me@joshma.com>
2011-06-12 22:49:25 -0700

    fixed history api - switching between employers has the illusion of changing pages

--- static/javascript/employer/employers_list.js
+++ static/javascript/employer/employers_list.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
     for (var i=0; i<employer_ids.length; i++) {
-        $('.employers_listing').eq(i).bind('click', {id: employer_ids[i], num: i}, loadEmployer)
+        $('.employers_listing').eq(i).bind('click', {id: employer_ids[i], i: i}, loadEmployer)
     }
 });
 
-function loadEmployer(e) {
-    var employer_id = e.data.id;
-    $.get(EMPLOYERS_LIST_EL_URL, {id: employer_id}, function(data) {
+function loadEmployer(e, noPush) {
+    var disablePush = noPush || false;
+    var id = e.data.id;
+    var i = e.data.i;
+    $.get(EMPLOYERS_LIST_EL_URL, {id: id}, function(data) {
         $('#employers_detail').html(data);
-        var stateObj = {id: employer_id};
-        history.pushState(stateObj, "employer "+employer_id, EMPLOYERS_LIST_URL+"?id="+employer_id);
+        if (!disablePush) {
+            var stateObj = {id: id, i: i};
+            history.pushState(stateObj, "employer "+id, EMPLOYERS_LIST_URL+"?id="+id);
+        }
         $('.selected').removeClass('selected');
-        $('.employers_listing').eq(e.data.num).addClass('selected');
+        $('.employers_listing').eq(i).addClass('selected');
     });
+}
+
+window.onpopstate = function(event) {
+    if (event.state != null) {
+        var stateObj = {id: event.state.id, i: event.state.i};
+        loadEmployer({data: stateObj}, true)
+    } else if ($('#isnotajax').val()=='false') {
+        var stateObj = {id: FIRST_EMPLOYER_ID, i: 0};
+        loadEmployer({data: stateObj}, true);
+    }
 }
--- static/javascript/events/events_list.js
+++ static/javascript/events/events_list.js
@@ -23,4 +23,5 @@ $(document).ready(function(){
             filterEvents();
         }
     }
+    $('#event_search_query').focus();
 });
