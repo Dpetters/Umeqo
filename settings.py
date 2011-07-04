2fe15f725c2579a47eb3c9b32998d7537a901e9c
Dmitrij Petters <dpetters@mit.edu>
2011-07-03 22:27:14 -0400

    Removed SESSION_EXPIRE_AT_BROWSER_CLOSE. This sets this option to False

--- settings.py
+++ settings.py
@@ -7,9 +7,6 @@
 import os
 ROOT = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
 
-# By default, a session expires when the browser is closed.
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-
 ACCOUNT_ACTIVATION_DAYS = 1 # One-day activation window;
 
 # Haystack Settings
