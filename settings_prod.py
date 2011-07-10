982b640f23e62d147a43205f6271e11dcb51845f
root <root@li323-63.members.linode.com>
2011-07-10 18:09:17 -0400

    commiting staging settings changes

--- settings_prod.py
+++ settings_prod.py
@@ -1,18 +1,21 @@
 import socket
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+COMPRESS = True
 
 def is_prod():
     return ['66.228.51.22'] == socket.gethostbyname_ex(socket.gethostname())[2]
 
-USE_LANDING_PAGE = is_prod()
+USE_LANDING_PAGE = True
 
 # 1 - Prod, 2 - Staging, 3 - Dev/Local
 if is_prod():
     SITE_ID = 1
+    DB_PASSWORD = 'H3rcul3s'
 else:
     SITE_ID = 2
+    DB_PASSWORD = 'Jamb4Juic3'
 
 ADMINS = (
     ("Dmitrij", "Dpetters91@gmail.com"),
@@ -28,7 +31,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'umeqo_main',                   # Or path to database file if using sqlite3.
         'USER': 'root',                         # Not used with sqlite3.
-        'PASSWORD': 'H3rcul3s',               # Not used with sqlite3.
+        'PASSWORD': DB_PASSWORD,                # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
