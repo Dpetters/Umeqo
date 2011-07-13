f3b767e9aad102b25078db9ae94ab9108148ca22
Dmitrij Petters <dpetters@mit.edu>
2011-07-12 20:03:40 -0400

    Removed a few print statements, committed local data

--- core/models.py
+++ core/models.py
@@ -176,11 +176,10 @@ class EventType(models.Model):
 
 if 'notification' in settings.INSTALLED_APPS:
     from notification import models as notification
-    print 'Creating notice types...'
     def create_notice_types(app, created_models, verbosity, **kwargs):
         notification.create_notice_type('new_event', 'New Event', 'an employer has created a new event')
         notification.create_notice_type('public_invite', 'Public Event Invite', 'an employer has invited you to an event')
 
     signals.post_syncdb.connect(create_notice_types, sender=notification)
 else:
-    print "Not creating notice types - notification app not found"
+    pass
