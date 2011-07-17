3f16e2a37a0591feff58c99648ecabbcf834e315
Dmitrij Petters <dpetters@mit.edu>
2011-07-17 18:03:36 -0400

    Fixed image resizing bug

--- core/model_helpers.py
+++ core/model_helpers.py
@@ -2,15 +2,15 @@ import datetime, Image
 from django.conf import settings
 
 def get_resume_filename(instance, filename):
-    return "%s%s_%s_%s_%s.pdf" % (settings.STUDENT_STUDENT_ROOT, instance.last_name.lower(), instance.first_name.lower(), str(instance.user).lower(), datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+    return "%s%s_%s_%s_%s.pdf" % (settings.STUDENT_STUDENT_PATH, instance.last_name.lower(), instance.first_name.lower(), str(instance.user).lower(), datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
 
 def get_campus_org_image_filename(instance, filename):
     extension = filename[filename.find('.'):]
-    return "%s%s_%s%s" % (settings.CORE_CAMPUS_ORG_ROOT, instance.name.replace(" ", "_").lower(), datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), extension)
+    return "%s%s_%s%s" % (settings.CORE_CAMPUS_ORG_PATH, instance.name.replace(" ", "_").lower(), datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), extension)
 
 def get_course_image_filename(instance, filename):
     extension = filename[filename.find('.'):]
-    return "%s%s_%s%s" % (settings.CORE_COURSE_ROOT, instance.name.replace(" ", "_").lower(), datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), extension)
+    return "%s%s_%s%s" % (settings.CORE_COURSE_PATH, instance.name.replace(" ", "_").lower(), datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), extension)
 
 def scale_down_image(image):
     filename = image.path
