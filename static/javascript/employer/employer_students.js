6ca3a405207d095aa4904254f7946c1d9c432084
Dmitrij Petters <dpetters@mit.edu>
2011-07-23 13:39:15 -0400

    added profile_create=True to the visible() method in the StudentManager. Fixed the order-results-by and results-per-page on the filtering page

--- static/javascript/employer/employer_students.js
+++ static/javascript/employer/employer_students.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     var page = 1;
     var student_list = $("#id_student_list option:selected").text();
     var ordering = $("#id_ordering option:selected").val();
+    console.log($("#id_results_per_page option:selected").val());
     var results_per_page = $("#id_results_per_page option:selected").val();
     var courses = [];
     var school_years = [];
--- student/managers.py
+++ student/managers.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class StudentManager(models.Manager):
     def visible(self):
-        return self.filter(user__is_active=True, user__userattributes__is_verified=True)
+        return self.filter(user__is_active=True, user__userattributes__is_verified=True, profile_created=True)
