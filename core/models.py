5a3ccf8483aebf961cf2b339403b6ea8d0fe93e5
Dmitrij <dpetters@mit.edu>
2011-08-14 03:50:33 -0400

    Uncommented a line I didn't mean to comment and fixed the tab that was messing up the admin save and go to next button for campus orgs.

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from notification import models as notification
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_syncdb
 
 from core.model_helpers import get_course_image_filename, get_course_thumbnail_filename
 from core import enums
@@ -172,4 +172,4 @@ def create_notice_types(app, created_models, verbosity, **kwargs):
     notification.create_notice_type('public_invite', 'Public Event Invite', 'an employer has invited you to an event')
     notification.create_notice_type('private_invite', 'Private Event Invite', 'an employer has invited you to an event')
 
-#signals.post_syncdb.connect(create_notice_types, sender=notification)
+post_syncdb.connect(create_notice_types, sender=notification)
