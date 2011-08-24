37a803b5c9f0fb1c94b0fe9bca7ef65d1efde439
Dmitrij Petters <dmitrij@umeqo.com>
2011-08-24 09:03:52 -0400

    another resumebook/event image naming change

--- employer/model_helpers.py
+++ employer/model_helpers.py
@@ -1,8 +1,8 @@
 import datetime
 
 def get_resume_book_filename(instance, filename):
-    return "%s/%s_%s_%s_%s.pdf" % (str(type(instance)._meta.replace(".", "/")), instance.recruiter.user.first_name.lower(), instance.recruiter.user.last_name.lower(), instance.recruiter.user, datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+    return "%s/%s_%s_%s_%s.pdf" % (str(type(instance)._meta).replace(".", "/"), instance.recruiter.user.first_name.lower(), instance.recruiter.user.last_name.lower(), instance.recruiter.user, datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
 
 def get_logo_filename(instance, filename):
     extension = filename[filename.find('.'):]
-    return "%s/%s_%s%s" % (str(type(instance)._meta.replace(".", "/")), instance.name.lower(), datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), extension)
+    return "%s/%s_%s%s" % (str(type(instance)._meta).replace(".", "/"), instance.name.lower(), datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), extension)
