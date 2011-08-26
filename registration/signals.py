421cd481562bc786dcc00d460d10f9d642026780
Joshua Ma <me@joshma.com>
2011-08-26 18:15:20 -0400

    clear attempts on successful login

--- registration/signals.py
+++ registration/signals.py
@@ -1,4 +1,5 @@
-from django.dispatch import Signal
+from django.contrib.auth import signals
+from django.dispatch import Signal, receiver
 
 
 # A new user has registered.
@@ -6,3 +7,8 @@ user_registered = Signal(providing_args=["user", "request"])
 
 # A user has activated his or her account.
 user_activated = Signal(providing_args=["user", "request"])
+
+@receiver(signals.user_logged_in)
+def clear_attempts(sender, request, user):
+    ip_address = request.META['HTTP_X_FORWARDED_FOR']
+    LoginAttempt.objects.all().filter(ip_address=ip_address).delete()
