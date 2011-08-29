0df9254878233eb93f78c3b52af292d0858ae272
Dmitrij Petters <dmitrij@umeqo.com>
2011-08-28 22:00:01 -0400

    Beginning of images in welcome block, is_verified bug fixed

--- registration/models.py
+++ registration/models.py
@@ -68,7 +68,7 @@ class UserAttributes(models.Model):
 @receiver(post_save, sender=User)
 def create_userattributes(sender, instance, created, raw, **kwargs):
     if created and not raw:
-        UserAttributes.objects.create(user=instance, is_verified=False)
+        UserAttributes.objects.create(user=instance, is_verified=True)
 
 
 class RegistrationProfile(models.Model):
