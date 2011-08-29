96651f3ee74a54c927842358ed02b5f3c41aac23
Dmitrij Petters <dmitrij@umeqo.com>
2011-08-29 00:54:19 -0400

    db password back to DB_PASSWORD

--- settings_prod.py
+++ settings_prod.py
@@ -41,7 +41,7 @@ STATIC_ROOT = "/var/www/static/"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'umeqo_main',                   # Or path to database file if using sqlite3.
+        'NAME': DB_PASSWORD,                   # Or path to database file if using sqlite3.
         'USER': 'root',                         # Not used with sqlite3.
         'PASSWORD': 'H3rcul3s',                # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
