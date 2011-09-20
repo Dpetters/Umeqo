48fb483e0df8b93ea9a48336468f8f7efd5f9b9e
Dmitrij Petters <dpetters@mit.edu>
2011-09-19 21:23:06 -0400

    fixed the script

--- core/management/commands/fill_attendees.py
+++ core/management/commands/fill_attendees.py
@@ -1,10 +1,16 @@
+from django.core.management.base import BaseCommand
+
 from events.models import Invitee
 from student.models import Student
-for i in Invitee.objects.all():
-    if not i.student:
-        try:
-            s = Student.objects.get(user__username=i.email)
-            i.student = s
-            i.save()
-        except Student.DoesNotExist:
-            pass
+
+class Command(BaseCommand):
+    
+    def handle(self, *args, **options):
+        for i in Invitee.objects.all():
+            if not i.student:
+                try:
+                    s = Student.objects.get(user__username=i.email)
+                    i.student = s
+                    i.save()
+                except Student.DoesNotExist:
+                    pass
