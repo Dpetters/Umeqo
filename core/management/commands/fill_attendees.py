50fa222a6bf38c9ae605518bfceaea1480ac8d59
Dmitrij Petters <dpetters@mit.edu>
2011-09-19 21:27:33 -0400

    oops Invitee should be Attendee

--- core/management/commands/fill_attendees.py
+++ core/management/commands/fill_attendees.py
@@ -1,12 +1,12 @@
 from django.core.management.base import BaseCommand
 
-from events.models import Invitee
+from events.models import Attendee
 from student.models import Student
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
-        for i in Invitee.objects.all():
+        for i in Attendee.objects.all():
             if not i.student:
                 try:
                     s = Student.objects.get(user__username=i.email)
