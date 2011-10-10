c2b5fce600231bae2c6e70079b93adebef67df15
Dmitrij Petters <dpetters@mit.edu>
2011-10-10 11:03:19 -0400

    tutorials and landing page change

--- core/admin.py
+++ core/admin.py
@@ -12,6 +12,21 @@ from core.models import Tutorial, CampusOrgType, Course, Language, SchoolYear, G
 
 class TutorialAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
+    
+    def response_change(self, request, obj):
+        """
+        Determines the HttpResponse for the change_view stage.
+        """
+        if request.POST.has_key("_viewnext"):
+            msg = (_('The %(name)s "%(obj)s" was changed successfully.') %
+                   {'name': force_unicode(obj._meta.verbose_name),
+                    'obj': force_unicode(obj)})
+            next = obj.__class__.objects.filter(id__gt=obj.id).order_by('name')[:1]
+            if next:
+                self.message_user(request, msg)
+                return HttpResponseRedirect("../%s/" % next[0].pk)
+        return super(TutorialAdmin, self).response_change(request, obj)
+    
 admin.site.register(Tutorial, TutorialAdmin)
 
 class LocationAdmin(admin.ModelAdmin):
@@ -76,6 +91,7 @@ class EmploymentTypeAdmin(admin.ModelAdmin):
     fields = ['name', 'sort_order']
     ordering = ('-last_updated',)
     date_hierarchy = 'last_updated'
+
 
 class CourseAdmin(admin.ModelAdmin):
     fieldsets = [
