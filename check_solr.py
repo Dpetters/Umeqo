52a9359a193a76c813993fda2b014f8f1872e9b4
Dmitrij Petters <dpetters@mit.edu>
2012-01-24 23:48:10 -0800

    check solr script fix #2

--- check_solr.py
+++ check_solr.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import time
 import os
 import settings
 import urllib2
@@ -17,10 +18,11 @@ except urllib2.URLError:
     os.chdir(solrdir)
     os.system('nohup java -jar start.jar > ../logs/solr-reboots.log 2>&1 &')
     print 'Restarted solr on ' + strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    time.sleep(2)
     try:
         solrpanel = urllib2.urlopen(settings.HAYSTACK_SOLR_URL)
     except urllib2.URLError:
-        send_html_mail("[Umeqo] Site #%s DOWN" % settings.SITE_ID, "Site #%s'w solr is down. You should probably check that shit out, bro.", managers)
+        send_html_mail("[Umeqo] Site #%s DOWN" % settings.SITE_ID, "Site #%s'w solr is down. You should probably check that shit out, bro." % settings.SITE_ID, managers)
 
 else:
     print 'Everything is fine! on ' + strftime("%Y-%m-%d %H:%M:%S", gmtime())
