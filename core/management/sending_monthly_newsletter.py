6d7de0c1cecefdc4fd3e5b0fabd03f1616f27a79
Dmitrij Petters <dpetters@mit.edu>
2012-02-06 07:49:32 -0500

    monthly newsletter r1

---
+++ core/management/sending_monthly_newsletter.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+from django.core.management.base import BaseCommand
+
+from events.models import Event
+
+class Command(BaseCommand):
+    
+    def handle(self, *args, **options):
+        for e in Event.objects.filter(type = EventType.objects.get(name="Hard Deadline")):
+            e.start_datetime = e.end_datetime
+            e.save()
+
+        for e in Event.objects.filter(type = EventType.objects.get(name="Rolling Deadline")):
+            e.start_datetime = datetime.now() - timedelta(weeks=1)
+            e.save()
