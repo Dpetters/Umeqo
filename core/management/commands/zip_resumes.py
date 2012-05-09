5ff82589389a3ad900476d6be2a4c5760b9914eb
Dmitrij Petters <dpetters@mit.edu>
2012-05-09 19:35:48 -0400

    zip files management command bug fix

--- core/management/commands/zip_resumes.py
+++ core/management/commands/zip_resumes.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from django.conf import settings as s
 from django.core.management.base import BaseCommand
 
+from core.file_utils import find_first_file
 from core.zip import create_zip
 from student.models import Student
 
@@ -21,7 +22,9 @@ def zip_resumes():
             for student in students:
                 file_name = "%s %s %s.pdf" % (student.first_name, student.last_name, student.user.email)
                 shutil.copyfile("%s/%s" % (s.MEDIA_ROOT, student.resume.name), "%s/%s" %(school_dir, file_name))
+            old_zip_file = find_first_file(s.MEDIA_ROOT, "%s.*.zip" % s.ALL_ZIPPED_RESUMES_FILENAME_START)
             create_zip(resume_dir, "", "%s/%s %s.zip" % (s.MEDIA_ROOT, s.ALL_ZIPPED_RESUMES_FILENAME_START, datetime.now().strftime("%Y-%b-%d %H-%M-%S")))
+            os.remove(old_zip_file)
     except Exception as e:
         print e
 
