e373bcff30eb225c07e5b1a65679676f413a7a65
Dmitrij Petters <dpetters@mit.edu>
2012-05-23 13:49:26 -0400

    updated student types script

--- core/management/commands/student_types.py
+++ core/management/commands/student_types.py
@@ -1,18 +1,18 @@
 from django.core.management.base import BaseCommand
 
-from student.models import Student, StudentType
+from student.models import Student, DegreeProgram
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        grad_phd = StudentType.objects.get(name="Graduate - PHD")
-        grad_masters = StudentType.objects.get(name="Graduate - Masters")
-        undergrad = StudentType.objects.get(name="Undergraduate")
+        grad_phd = DegreeProgram.objects.get(name="Research Doctorate Degree")
+        grad_masters = DegreeProgram.objects.get(name="Master's Degree")
+        undergrad = DegreeProgram.objects.get(name="Bachelors Degree")
         
         for student in Student.objects.filter(profile_created=True):
             if student.school_year.name == "Graduate - PHD":
-                student.type = grad_phd
+                student.degree_program = grad_phd
             elif student.school_year.name == "Graduate - Masters":
-                student.type = grad_masters
+                student.degree_program = grad_masters
             else:
-                student.type = undergrad
+                student.degree_program = undergrad
             student.save() 
