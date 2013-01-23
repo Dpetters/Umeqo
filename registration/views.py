b586ca85a04f33c96a583a5a70ed2bd60b4e749d
Dmitrij Petters <Dpetters91@gmail.com>
2013-01-22 21:08:45 -0800

    allow users to resend activation emails

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import re
 import urllib
 import urllib2
 
@@ -7,9 +8,9 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required,  user_passes_test
 from django.contrib.auth.models import User, update_last_login
 from django.contrib.auth.signals import user_logged_in
-from django.contrib.auth.views import login as auth_login_view, logout as auth_logout_then_login_view
+from django.contrib.auth.views import login as auth_login_view, logout as auth_logout_then_login_view, password_reset as auth_password_reset
 from django.contrib.sessions.models import Session
-from django.contrib.sites.models import get_current_site
+from django.contrib.sites.models import Site, get_current_site
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import redirect
@@ -18,12 +19,13 @@ from django.utils import simplejson
 from campus_org.models import CampusOrg
 from core.decorators import render_to
 from core.forms import EmailAuthenticationForm as AuthenticationForm, SuperLoginForm
+from core import messages as m
 from core.view_helpers import get_ip
 from core.signals import us_user_logged_in
 from events.models import Event
-from registration.models import LoginAttempt
+from registration.models import LoginAttempt, RegistrationProfile
 from registration.backend import RegistrationBackend
-from registration.forms import PasswordChangeForm
+from registration.forms import PasswordResetForm, PasswordChangeForm
 from notification.models import NoticeType
 from employer.models import Employer
 from notification import models as notification
@@ -53,6 +55,14 @@ def login(request, template_name="login.html", authentication_form=Authenticatio
         'invalid_captcha': False,
         'RECAPTCHA_PUBLIC_KEY': s.RECAPTCHA_PUBLIC_KEY,
     })
+    
+    form = authentication_form(data=request.POST)
+    if not form.is_valid():
+        if re.search(m.not_activated, str(form.errors)):
+            extra_context.update({'show_resend_activation_email_form':True})
+        else:
+            extra_context.update({'show_resend_activation_email_form':False})
+    
     if request.method == 'POST' and login_attempts > 10:
         captcha_params = urllib.urlencode({
             'privatekey': s.RECAPTCHA_PRIVATE_KEY,
@@ -90,7 +100,16 @@ def login(request, template_name="login.html", authentication_form=Authenticatio
     else:
         us_user_logged_in.send(sender=request.user.__class__, request=request, user=request.user)
         return response
-    
+
+
+def password_reset(request, password_reset_form = PasswordResetForm, template_name="password_reset_form.html", email_template_name="password_reset_email.html", extra_context={}):
+    form = PasswordResetForm(data=request.POST)
+    if not form.is_valid():
+        if re.search(m.not_activated, str(form.errors)):
+            extra_context.update({'show_resend_activation_email_form':True})
+        else:
+            extra_context.update({'show_resend_activation_email_form':False})
+    return auth_password_reset(request, password_reset_form = password_reset_form, template_name=template_name, email_template_name=email_template_name, extra_context = extra_context)
 
 @render_to('super_login.html')
 @user_passes_test(lambda x: x.is_superuser)
@@ -112,6 +131,15 @@ def super_login(request, form_class = SuperLoginForm,  extra_context=None):
     context.update(extra_context or {})
     return context
 
+def resend_activation_email(request, extra_context=None):
+    if Site._meta.installed:
+        site = Site.objects.get_current()
+    else:
+        site = RequestSite(request)
+    profile = RegistrationProfile.objects.get(user__email=request.POST.get("email"))
+
+    profile.send_activation_email(site, first_name=profile.user.first_name, last_name=profile.user.last_name)
+    return redirect(reverse('student_registration_complete'))
 
 @login_required
 def password_change(request, password_change_form=PasswordChangeForm, extra_context=None):
