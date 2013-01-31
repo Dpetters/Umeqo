75e7fae382100e8e325b204e21b1461cdfba66b0
Dmitrij Petters <Dpetters91@gmail.com>
2013-01-30 21:16:35 -0800

    event rsvp csrf bug fix

--- static/js/base.js
+++ static/js/base.js
@@ -337,6 +337,7 @@ $(document).ajaxSend(function(event, xhr, settings) {
     function safeMethod(method) {
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
     }
+    console.log(xhr);
     if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
         xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
     }
